310ee4d3d974cdab30b08ba9fdbc3ac00c77a8c3
onlynyxs <neeksonsh1@gmail.com>
2026-02-28 07:58:04 +0000

    Minor Deployment fixes part 2

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  output: process.env.GITHUB_ACTIONS ? 'export' : undefined,
   reactCompiler: true,
   trailingSlash: true,
   images: {
